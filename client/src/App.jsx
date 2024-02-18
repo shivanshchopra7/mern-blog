1152f151aa340f0017177ed216ef1fbe925a0de7
Shivansh Chopra <gauravshivansh7@gmail.com>
2024-02-19 01:37:22 +0530

    completing footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Projects from './pages/Projects';
 import Header from './components/Header';
+import Footer from './components/Footer';
 function App() {
   return (
     <BrowserRouter>
@@ -20,7 +21,9 @@ function App() {
 <Route path='/sign-in' element={<SignIn />} />
 <Route path='/sign-up' element={<SignUp />} />
 <Route path='/projects' element={<Projects />} />
+
     </Routes>
+    <Footer />
     </BrowserRouter>
   )
 }
